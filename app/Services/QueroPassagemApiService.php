e1ccb4a363a8fbb82f7b018fbe0997c1d5b45686
Pablo Emanuel <maneu471@gmail.com>
2024-10-06 17:41:44 -0300

    feat(Service): new service to call external reqs

---
+++ app/Services/QueroPassagemApiService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class QueroPassagemApiService
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => config('queropassagem.base_url'),
+            'headers' => [
+                'Content-Type'  => 'application/json',
+                'Accept'        => 'application/json',
+                'Authorization' => 'Basic ' . base64_encode(config('queropassagem.user') . ':' . config('queropassagem.password')),
+            ],
+        ]);
+    }
+
+    public function getCompanies()
+    {
+        try {
+            $response = $this->client->request('GET', 'companies');
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            Log::error('Erro ao acessar a API: ' . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function getStops()
+    {
+        try {
+            $response = $this->client->request('GET', 'stops');
+
+            $stops = json_decode($response->getBody()->getContents(), true);
+            $stops = $this->filterStops($stops);
+            return $stops;
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            Log::error('Erro ao acessar a API: ' . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function filterStops($stops)
+    {
+        $filteredStops = [];
+        foreach ($stops as $stop) {
+            if (Str::contains($stop['name'], ['SP', 'PR'])) {
+                $filteredStops[] = $stop;
+            }
+        }
+        return $filteredStops;
+    }
+}
---
+++ config/queropassagem.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'base_url' => env('QP_BASE_URL'),
+    'user'     => env('QP_USER'),
+    'password' => env('QP_PASSWORD'),
+    'affiliate_code' => env('QP_AFFILIATE_CODE'),
+];
